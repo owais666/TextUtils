96eaa6f930ec5e10dcdab63842b3b71d0a8359d9
Awais <72447066+owais666@users.noreply.github.com>
2024-01-21 15:37:04 +0530

    Add files via upload

---
+++ build/precache-manifest.7c75ff74fdc558e2e736934287ac4459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0293e05e96adb7cc78dae59892d61679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059d78913b499735700d",
+    "url": "/static/css/main.2e3cfc66.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "059d78913b499735700d",
+    "url": "/static/js/main.4f77e432.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
